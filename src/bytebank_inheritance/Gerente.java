38adf3b8a4338a2998c3c9ce985a5be43bd76525
Jhonatan Sena <jcostasenna@hotmail.com>
2023-02-26 12:04:03 -0300

    feat: add gerenete class

---
+++ src/bytebank_inheritance/Gerente.java
@@ -0,0 +1,12 @@
+package bytebank_inheritance;
+
+public class Gerente extends Funcionario {
+
+	private int senha;
+	
+	public boolean autentica(int senha) {
+		return this.senha == senha ? true : false;
+	}
+	
+	
+}
