ff6148df2d0ed233e8064ce3f5bf64b5f2f1a9d7
Jhonatan Sena <jcostasenna@hotmail.com>
2023-02-27 21:20:18 -0300

    feat: add abstract class funcionario autenticavel to isolate autenticate method

---
+++ src/bytebank_inheritance/FuncionarioAutenticavel.java
@@ -0,0 +1,15 @@
+package bytebank_inheritance;
+
+public abstract class FuncionarioAutenticavel extends Funcionario {
+
+	private int senha;
+
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+	
+	public boolean autentica(int senha) {
+		return this.senha == senha ? true : false;
+	}
+	
+}
