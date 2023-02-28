05c46a93afcdbe3d99828ecdb5d16b663dc46c40
Jhonatan Sena <jcostasenna@hotmail.com>
2023-02-27 21:20:49 -0300

    feat: some adjust on the code

--- src/bytebank_inheritance/Gerente.java
+++ src/bytebank_inheritance/Gerente.java
@@ -1,21 +1,8 @@
 package bytebank_inheritance;
 
-public class Gerente extends Funcionario {
-
-	private int senha;
-
-	public boolean autentica(int senha) {
-		return this.senha == senha ? true : false;
-	}
-	
+public class Gerente extends FuncionarioAutenticavel {
 
 	
-	public boolean autentica(int senha, String login) {
-		return this.senha == senha ? true : false;
-	}
-
-
-
 	@Override
 	public double getBonificacao() {
 		// TODO Auto-generated method stub
